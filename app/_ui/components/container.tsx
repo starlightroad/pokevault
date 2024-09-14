254683345c0d7b453afaf2a75ed9b6ec6db2ed54
starlightroad <ortizge92@gmail.com>
2024-09-14 18:50:37 -0400

    Change the max width to 1024px

--- app/_ui/components/container.tsx
+++ app/_ui/components/container.tsx
@@ -1,3 +1,3 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto w-full max-w-screen-sm px-4 md:px-5">{children}</div>;
+  return <div className="mx-auto w-full max-w-screen-lg px-4 md:px-5">{children}</div>;
 }
